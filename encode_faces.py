a73ab0ed9b334386ef3d6dc94153009ebf2cbb80
Jesus Moncada Ramirez <jemonra@gmail.com>
2022-02-13 21:22:57 +0100

    Actualizado requirements y codigo

--- encode_faces.py
+++ encode_faces.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from imutils import paths
 import face_recognition
 import argparse
@@ -11,7 +10,10 @@ if __name__ == '__main__':
 
     t = time.time()
 
-    # construct the argument parser and parse the arguments
+    # Arguments:
+    # -i dataset of user images
+    # -e encodings file to be created with encodings of dataset
+    # -d detection method cnn or hog
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--dataset", required=True,
                     help="path to input directory of faces + images")
@@ -21,38 +23,33 @@ if __name__ == '__main__':
                     help="face detection model to use: either `hog` or `cnn`")
     args = vars(ap.parse_args())
 
-    # grab the paths to the input images in our dataset
     print("[INFO] Cuantificando caras...")
     imagePaths = list(paths.list_images(args["dataset"]))
-    # initialize the list of known encodings and known names
+
     knownEncodings = []
     knownNames = []
 
-    # loop over the image paths
     for (i, imagePath) in enumerate(imagePaths):
-        # extract the person name from the image path
+
         name = imagePath.split(os.path.sep)[-2]
         print("[INFO] Procesando imagen {}/{} de {}".format(i + 1,
                                                             len(imagePaths), name))
-        # load the input image and convert it from BGR (OpenCV ordering)
-        # to dlib ordering (RGB)
-        image = cv2.imread(imagePath)
-        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # detect the (x, y)-coordinates of the bounding boxes
-        # corresponding to each face in the input image
+        image = cv2.imread(imagePath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # BGR -> RGB
+
+        # Detectar coordenadas (x, y) de los bounding boxes correspondientes a cada cara en la imagen de entrada
         boxes = face_recognition.face_locations(rgb,
                                                 model=args["detection_method"])
-        # compute the facial embedding for the face
+        # Computa el vector de características
         encodings = face_recognition.face_encodings(rgb, boxes)
-        # loop over the encodings
-        for encoding in encodings:
-            # add each encoding + name to our set of known names and
-            # encodings
+
+        for encoding in encodings: # Por cada encoding (puede haber más de uno si en la imagen había más de una cara)
+            # Añade cada encoding + nombre al set de nombres conocidos y encodings
             knownEncodings.append(encoding)
             knownNames.append(name)
 
-    # dump the facial encodings + names to disk
+    # Carga los encodings al disco
     print("[INFO] Serializando encodings...")
     data = {"encodings": knownEncodings, "names": knownNames}
     f = open(args["encodings"], "wb")
